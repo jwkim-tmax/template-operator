48066ea01db814f51e7428d672ab49492afcd71f
JaewonKim <huhu420@naver.com>
2020-08-03 10:14:28 +0900

    initial commit

---
+++ pkg/apis/addtoscheme_tmax_v1.go
@@ -0,0 +1,8 @@
+package apis
+
+import v1 "k8s.io/api/apps/v1"
+
+func init() {
+	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
+	AddToSchemes = append(AddToSchemes, v1.SchemeBuilder.AddToScheme)
+}
